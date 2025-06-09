99c3247a57a69b87ec096041bbd34d27c04ab31b
deorbil <80085032+deorbil@users.noreply.github.com>
2025-06-09 23:31:02 +0700

    feat(wezterm): add config

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,13 @@
+local wezterm = require("wezterm")
+
+return {
+	color_scheme = "Catppuccin Mocha",
+	colors = {
+		background = "#000000",
+	},
+	default_cursor_style = "BlinkingBar",
+	font = wezterm.font("CaskaydiaCove Nerd Font"),
+	hide_tab_bar_if_only_one_tab = true,
+	line_height = 1.2,
+	window_background_opacity = 0.8,
+}
