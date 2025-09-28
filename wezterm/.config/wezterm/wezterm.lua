aba196a643e513221e61323b9d91aeb19aaa1858
deorbil <80085032+deorbil@users.noreply.github.com>
2025-09-28 11:31:36 +0700

    feat(wezterm): hide tab bar

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -3,5 +3,6 @@ local wezterm = require("wezterm")
 return {
 	color_scheme = "Catppuccin Mocha",
 	font = wezterm.font("CaskaydiaCove Nerd Font"),
+	hide_tab_bar_if_only_one_tab = true,
 	line_height = 1.2,
 }
