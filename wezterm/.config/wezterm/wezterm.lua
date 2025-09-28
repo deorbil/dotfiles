08032d64de2399a7df5a413de2f5ecf3ccb72739
deorbil <80085032+deorbil@users.noreply.github.com>
2025-09-28 11:32:16 +0700

    feat(wezterm): set background and opacity

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,7 +2,11 @@ local wezterm = require("wezterm")
 
 return {
 	color_scheme = "Catppuccin Mocha",
+	colors = {
+		background = "#000000",
+	},
 	font = wezterm.font("CaskaydiaCove Nerd Font"),
 	hide_tab_bar_if_only_one_tab = true,
 	line_height = 1.2,
+	window_background_opacity = 0.8,
 }
