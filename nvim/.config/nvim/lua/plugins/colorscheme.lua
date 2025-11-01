09d28a6f047310afec8b35367144eeb6cf9341eb
deorbil <80085032+deorbil@users.noreply.github.com>
2025-11-01 19:03:56 +0700

    feat(nvim): set transparent background

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,5 +1,14 @@
 return {
   {
+    "catppuccin/nvim",
+    opts = {
+      transparent_background = true,
+      float = {
+        transparent = true,
+      },
+    },
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "catppuccin",
